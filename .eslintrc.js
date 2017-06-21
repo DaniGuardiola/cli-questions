ff29c117b6dcadef1e04a7d91a3edd4ef73ad8dd
Dani Guardiola <contact@daniguardiola.me>
2017-06-21 14:43:07 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "airbnb/base",
+  "installedESLint": true,
+  "rules": {
+    "no-param-reassign": ["error", { "props": false }],
+    "no-console": 0,
+    "max-len": 0,
+    "comma-dangle": 0,
+    "no-shadow": 0,
+    "global-require": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "arrow-body-style": 0,
+    "class-methods-use-this": 0,
+    "no-mixed-operators": 0,
+    "no-plusplus": 0,
+    "camelcase": 0,
+  }
+};
---
+++ cli-questions.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+const Promise = require('bluebird');
+const readline = require('readline-sync');
+require('colors');
+
+/*
+q   ->  question
+qs  ->  questions
+a   ->  answer
+as  ->  answers
+cb  ->  callback
+ */
+
+const askCb = (q, cb) => {
+  const text = q.text || q; // If no text property, direct question string is assumed
+  const optional = q.optional && !q.default; // Optional only if no default defined
+  const defaultText = q.default ? `(${q.default}) `.blue : ''; // Default value text
+  const display = `${optional ? '(opt) '.blue : ''}${text} ${defaultText}`.bold; // Text to display
+
+  const data = q.bool ? // Ask for input
+    readline.keyInYN(`${display} ${'[y/n]'.green}`) : // Text
+    readline.question(display, { hideEchoBack: q.hidden === true }); // Boolean (y/n)
+
+  if (!optional && !q.default && data === '') return askCb(q, cb); // Ask again if not optional
+  const a = data !== '' ? data : (q.default || ''); // Use default if empty input
+  cb(a); // Return answer
+};
+
+const askPromise = q => (new Promise(resolve => askCb(q, resolve))); // Promisified askCb
+
+const ask = (q, cb) => (cb ? askCb(q, cb) : askPromise(q)); // Route depending on callback
+
+const toArray = qs => _.values(_.mapValues(qs, (val, key) => {
+  val.id = key;
+  return val;
+}));
+
+const toObject = (qs, as) => {
+  const obj = {};
+  _.each(qs, (val, index) => {
+    obj[val.id] = as[index];
+  });
+  return obj;
+};
+
+const askMany = (q, cb) => {
+  const qs = toArray(q); // For mapSeries
+  return Promise.mapSeries(qs, askPromise) // Ask questions
+    .then(as => toObject(qs, as)) // For final output
+    .tap(as => (cb ? cb(as) : '')); // Callback support
+};
+
+module.exports = { ask, askMany };
